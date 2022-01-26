c3d376a66e99177fd5a7322d0bde41b93ce40935
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2022-01-26 16:04:34 +0000

    Changed the output once again for testing

--- Software_Code/index.php
+++ Software_Code/index.php
@@ -86,14 +86,12 @@ $sql = "SELECT * FROM test";
 $rows = array();
 $result = $db->query($sql);
 while ($row = $result->fetch_array()) {
-    echo $result;
-
+  $rows[] = $row;
 }
 
 
-
-  //Closes the database connection
-  mysql_close($db);
+  header('Content-type: application/json');
+  echo json_encode($rows);
 
 ?>
 
