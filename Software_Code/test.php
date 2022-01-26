e161cdd0adf8bac8d779fd235d9d5878b57c3535
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2022-01-26 16:30:06 +0000

    printing the table

--- Software_Code/test.php
+++ Software_Code/test.php
@@ -5,21 +5,13 @@ include("dbconnect.php");
 
 $sql = "SELECT * FROM test";
 
-$rows = array();
 $result = $db->query($sql);
 while ($row = $result->fetch_array()) {
-    $rows[] = $row;
-    echo $row["name"];
+    echo $row["name"], "\n";
 }
-
-  header('Content-type: application/json');
-  echo json_encode($rows);
-
   //Closes the database connection
   mysql_close($db);
 
 ?>
 
-
-<html> haha quirky </html>
 
