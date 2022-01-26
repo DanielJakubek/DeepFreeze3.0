5594bdf1c64293ac941ea30c5e255187f7b17a98
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2022-01-26 14:27:12 +0000

    test is testing is getting routed to

--- test.php
+++ test.php
@@ -1,20 +1,4 @@
-<?php
-include("dbconnect.php");
 
-$nameID = $params['id'];
-
-$sql = "SELECT * FROM test WHERE nameID='". $nameID ."'";
-
-$rows = array();
-$result = $db->query($sql);
-while ($row = $result->fetch_array()) {
-    $rows[] = $row;
-}
-
-  header('Content-type: application/json');
-  echo json_encode($rows);
-
-?>
 
 
 <html>  asdad </html>
