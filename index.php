477d0c5aedbf2da936588f378ea5b28af846b0b1
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2022-01-26 14:02:44 +0000

    readded the code as it broke things

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php
+
+define('INCLUDE_DIR', dirname(__FILE__) . '/inc/');
+
+$rules = array(
+    //
+    //API Routes
+    'test' => "/api/test",
+
+);
+
+$uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
+$uri = '/' . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), '/');
+$uri = urldecode($uri);
+
+foreach ($rules as $action => $rule) {
+    if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
+        include(INCLUDE_DIR . $action . '.php');
+        exit();
+    }
+}
+
+// nothing is found so handle the 404 error
+include(INCLUDE_DIR . '404.php');
+
+?>
 
 <!doctype html>
 <html lang="en">
