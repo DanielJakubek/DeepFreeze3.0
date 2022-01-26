751b894a89ec7dbb01710caeb3ffdb38e788589a
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2022-01-26 15:45:44 +0000

    trying to print data

--- test.php
+++ test.php
@@ -9,11 +9,10 @@ $rows = array();
 $result = $db->query($sql);
 while ($row = $result->fetch_array()) {
     $rows[] = $row;
+    echo "rows: ", $rows;
 
 }
 
-  header('Content-type: application/json');
-  echo json_encode($rows);
 
   //Closes the database connection
   mysql_close($db);
