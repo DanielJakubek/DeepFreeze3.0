240800b18bd258310f92e41405a3450de6347cc4
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2022-01-26 12:24:59 +0000

    The index page with it's css and js

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+//Test popup
+function popupInnti(){
+  alert("HAHA");
+}
+
+
+//What we want to do
+
+
+//On button click, we want to add a new div, either a new carousel or paragraph???
+
+
+
+function addNewItem(){
+
+
+}
