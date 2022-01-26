40119f5bdbb1c18c152be1b2b6781579d3eec3de
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2022-01-26 16:42:44 +0000

    Got the images to work from the db. LESGO

--- Software_Code/test.php
+++ Software_Code/test.php
@@ -7,7 +7,6 @@ $sql = "SELECT * FROM images";
 
 $result = $db->query($sql);
 while ($row = $result->fetch_array()) {
-    echo $row["ImageName"], "\n";
     echo '<img src="data:image/jpeg;base64,'.base64_encode($row['ImageName']).'"/>';
 }
   //Closes the database connection
