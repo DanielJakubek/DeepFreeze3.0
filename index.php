2bc807eaf246e0206102e8496f92988b7fd2db5f
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2022-01-26 14:16:41 +0000

    removed the routing

--- index.php
+++ index.php
@@ -1,31 +1,3 @@
-<?php
-
-define('INCLUDE_DIR', dirname(__FILE__) . '/inc/');
-
-$rules = array(
-    
-    //API Routes
-    'test' => "/api/test",
-    'admin_login' => "/api/admin",
-
-);
-
-$uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
-$uri = '/' . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), '/');
-$uri = urldecode($uri);
-
-foreach ($rules as $action => $rule) {
-    if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
-        include(INCLUDE_DIR . $action . '.php');
-        exit();
-    }
-}
-
-// nothing is found so handle the 404 error
-include(INCLUDE_DIR . '404.php');
-
-?>
-
 <!doctype html>
 <html lang="en">
 
