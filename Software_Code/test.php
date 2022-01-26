894dd0b230100ed51e0e626774b0377f21b5364e
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2022-01-26 16:39:31 +0000

    testing printing img

--- Software_Code/test.php
+++ Software_Code/test.php
@@ -8,6 +8,7 @@ $sql = "SELECT * FROM images";
 $result = $db->query($sql);
 while ($row = $result->fetch_array()) {
     echo $row["ImageName"], "\n";
+    echo '<img src="data:image/jpeg;base64,'.base64_encode($row['ImageName']).'"/>';
 }
   //Closes the database connection
   mysql_close($db);
