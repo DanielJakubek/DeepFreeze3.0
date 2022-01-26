c0d98b4cf21fffb2d104bb1578b403dd55bead4f
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2022-01-26 14:32:16 +0000

    Saw an error with the sql statement, fixed it

--- test.php
+++ test.php
@@ -3,7 +3,18 @@
 
 include("dbconnect.php");
 
+$nameID = $params['id'];
 
+$sql = "SELECT * FROM test";
+
+$rows = array();
+$result = $db->query($sql);
+while ($row = $result->fetch_array()) {
+    $rows[] = $row;
+}
+
+  header('Content-type: application/json');
+  echo json_encode($rows);
 
 ?>
 
