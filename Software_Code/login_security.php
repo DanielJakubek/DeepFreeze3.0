d840421165509b8482616a905bea7e26a1d0aaec
DanielJakubek <56348383+DanielJakubek@users.noreply.github.com>
2022-01-28 12:24:40 +0000

    Moved php files outside the software code folder into it and replaced the old files with new ones.

--- Software_Code/login_security.php
+++ Software_Code/login_security.php
@@ -1,15 +1,58 @@
     <?php
     
+    if (!isset($_SESSION)) { session_start(); }
+
+    function query($id){
+        include("dbConnect.php");
+        $sql = "SELECT * FROM accounts WHERE username LIKE '$id'";
+    
+        $rows = array();
+        $result = $db->query($sql);
+        while ($row = $result->fetch_assoc()) {
+        $rows[] = $row;
+        }
+        $db->close();
+        return $rows;
+    }
+    
+
+    $valid_session = false;
+    $username = '';
+    $password = '';
+
     if (isset($_POST['username'])) {
-        echo "This is Button1 that is selected";
+        $_SESSION["username"] = $_POST['username']; 
+        $username = $_SESSION["username"];
     }
 
     if (isset($_POST['password'])) {
-        $password = crypt($_POST['password'], "FrozenBeans");
-        echo "your password is ", $password, "Get pwned n00b >:)";
+        $password = crypt($_POST['password'], "$username");
+    }
+    
+    $get_user = [];
+    $get_user = query($username);
+
+    if(count($get_user) > 0)
+    {
+        if($password == $get_user[0]['password'])
+        {
+            $valid_session = true;
+            echo "You're now logged in, Please wait...";
+            echo $valid_session;
+        }
     }
 
+    if($valid_session != true)
+    {
+        header("Location: admin_login.php");
+        die();
+    }
+    else if($valid_session == true)
+    {
+        $_SESSION["validsession"] = $valid_session;
+        header("Location: admin_page.php");
+        die();
+    }
     
-
 
     ?>
